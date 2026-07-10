609801d9784bf77bca7adaa8ccc65a8ee065a935
franjefriten <fj.friastenza@gmail.com>
2026-07-10 23:36:32 +0200

    chore: furst implementation of monitor_tool

---
+++ core/decorators.py
@@ -0,0 +1,53 @@
+import functools
+from typing import Callable, Any
+import time
+import asyncio
+from core.registry import registry
+from core.event import MCPEvent
+import inspect
+from utils.logger import logger
+
+
+def monitor_tool(func: Callable):
+
+    is_couroutine = inspect.iscoroutinefunction(func)
+
+    @functools.wraps(func)
+    async def async_wrapper(is_couroutine=is_couroutine, **kwargs):
+        return await _execute_and_monitor(func, **kwargs, is_couroutine=is_couroutine)
+    
+    @functools.wraps(func)
+    def sync_wrapper(is_couroutine=is_couroutine, **kwargs):
+        return _execute_and_monitor(func, **kwargs, is_couroutine=is_couroutine)
+
+    return async_wrapper if is_couroutine else sync_wrapper
+    
+
+def _execute_and_monitor(func: Callable, *args, is_couroutine: bool, **kwargs) -> Any:
+    start_time = time.perf_counter()
+
+    event_data = {
+        "tool_name": func.__name__,
+        "arguments": kwargs,
+        "timestamp": start_time,
+    }
+
+    try:
+        if is_couroutine:
+            result = asyncio.run_coroutine_threadsafe(func(*args, **kwargs))
+        else:
+            result = func(*args, **kwargs)
+        event_data["status"] = "success"
+        event_data["result"] = result
+    
+    except Exception as e:
+        event_data["status"] = "error"
+        event_data["error"] = str(e)
+        raise e
+    finally:
+        end_time = time.perf_counter()
+        delta = end_time - start_time
+        event_data["delta"] = delta
+        event = MCPEvent(**event_data)
+        registry.dispatch(event)
+        logger.info(f"Monitor tool: {func.__name__} tool with duration {delta:.4f} Event dispatched: {event.model_dump_json()}")
