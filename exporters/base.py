d5c5ad450ec38b0a9034cf2ea31d295489bcd55d
franjefriten <fj.friastenza@gmail.com>
2026-07-18 23:08:32 +0200

    feat: implement HTTP exporters with batch export functionality and enhance console logging

--- exporters/base.py
+++ exporters/base.py
@@ -4,8 +4,9 @@ This module provides a base class for all exporters, which can be extended to cr
 """
 from abc import ABC, abstractmethod
 from core.event import MCPEvent
-from typing import Callable
+from typing import Callable, List, Optional
 from tenacity import retry, retry_if_exception, stop_after_attempt, wait_exponential
+import httpx
 
 class BaseExporter(ABC):
     """
@@ -18,9 +19,62 @@ class BaseExporter(ABC):
         Export the given event to the desired format.
 
         :param event: The event to be exported.
-        :return: The exported data in the desired format.
         """
         pass
+
+    async def export_batch(self, event_batch: List[MCPEvent]) -> None:
+        """
+        Export the given batch of events to the desired format.
+
+        :param event_batch: The batch of events to be exported 
+        """
+        for event in event_batch:
+            await self.export(event)
+
+
+class HTTPBaseExporter(BaseExporter):
+    """
+    Base class for http exporters, uses httpx
+    """
+
+    def __init__(
+        self, 
+        url: str, 
+        auth: Optional[tuple | httpx.Auth] = None, 
+        headers: Optional[dict] = None
+    ):
+        self.url = url
+        self.headers = headers or {}
+        self.auth = auth
+        self.client: Optional[httpx.AsyncClient] = None
+
+    async def connect(self):
+        self.client = httpx.AsyncClient(
+            auth=self.auth,
+            headers=self.headers,
+            base_url=self.url,
+            timeout=1.0
+        )
+    
+    async def close(self):
+        if self.client:
+            self.client.aclose()
+            self.client = None
+
+    @abstractmethod
+    async def export(self, event: MCPEvent):
+        """
+        Abstract method of exportation
+        """
+        pass
+    
+    async def export_batch(self, event_batch: List[MCPEvent]):
+        """
+        Export in batches
+        """
+        for event in event_batch:
+            await self.export(event)
+
 
 def with_retry(func: Callable):
     return retry(
--- exporters/console.py
+++ exporters/console.py
@@ -5,9 +5,16 @@ Exporter module for console, used mostly for debugging purposes
 from exporters.base import BaseExporter
 from core.event import MCPEvent
 from utils.logger import logger
+from typing import List
 
 class ConsoleExporter(BaseExporter):
     def export(self, event: MCPEvent) -> None:
         status_icon = "✅" if event.status == "success" else "❌"
         logger.info(f"{status_icon} [Exec time: {event.delta}] {event.tool_name} - Status: {event.status}, Args: {event.args}, Result: {event.result}")
+    
+    def export_batch(self, event_batch: List[MCPEvent]) -> None:
+        for event in event_batch:
+            status_icon = "✅" if event.status == "success" else "❌"
+            logger.info(f"{status_icon} [Exec time: {event.delta}] {event.tool_name} - Status: {event.status}, Args: {event.args}, Result: {event.result}")
+
         
