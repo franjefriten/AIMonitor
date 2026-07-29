cc8e0d40449a2fe0826ff7dfa507a703b3549a9c
franjefriten <fj.friastenza@gmail.com>
2026-07-29 21:25:17 +0200

    feat: enhance AIMonitorSettings with logging for YAML/JSON loading, add retries policy config and add validation for max file size

--- configs/config.py
+++ configs/config.py
@@ -6,6 +6,8 @@ import yaml
 import aiofiles
 import json
 from functools import lru_cache
+from urllib import parse
+from utils.logger import logger
 
 
 _VALID_URLS = ["mongodb://", "redis://", "postgres://", "http://", "https://"]
@@ -41,13 +43,14 @@ class AIMonitorSettings(BaseSettings):
     prometheus_url: Optional[HttpUrl] = Field(default="http://localhost:9000", description="prometheus url to use", validation_alias="AIMONITOR_PROMETHEUS_URL")
     sqlite_uri: Optional[FilePath] = Field(default="./sqlite_aimonitor.sqlite", description="sqlite path to use")
     max_mb_per_file: Optional[float] = Field(default=10.0, description="max file size in megabytes")
-    level_log: Literal["error", "warning", "info"]
+    retries_policy: Optional[int] = Field(default=3, description="number of retries for exporters")
 
 
     async def load_from_yaml(self, yaml_file_path: str | Path) -> None:
         if isinstance(yaml_file_path, str):
             yaml_file_path = Path(yaml_file_path)
         if not yaml_file_path.exists():
+            logger.error("YAML config file was not found")
             raise FileNotFoundError("YAML config file was not found")
         
         async with aiofiles.open(file=yaml_file_path) as file:
@@ -60,12 +63,15 @@ class AIMonitorSettings(BaseSettings):
             updated_instance = self.__class__(**current_data)
             for field, value in updated_instance:
                 setattr(self, field, value)
+        
+        logger.info("YAML config file was loaded into aimonitor vars")
     
     
     async def load_from_json(self, json_file_path: str | Path) -> None:
         if isinstance(json_file_path, str):
             json_file_path = Path(json_file_path)
         if not json_file_path.exists():
+            logger.error("JSON config file was not found")
             raise FileNotFoundError("JSON config file was not found")
         
         async with aiofiles.open(file=json_file_path) as file:
@@ -78,13 +84,33 @@ class AIMonitorSettings(BaseSettings):
             updated_instance = self.__class__(**current_data)
             for field, value in updated_instance:
                 setattr(self, field, value)
+        
+        logger.info("JSON config file was loaded into aimonitor vars")
 
     
     @field_validator("redis_url", "mongobd_url", "postgres_url", mode="before")
     @classmethod
     def validate_urls(cls, v: Optional[str]) -> Optional[str]:
+        try:
+            parse.urlparse(url=v)
+        except AttributeError:
+            logger.error(f"Invalid url for service: {v}")
+            raise AttributeError(f"Invalid url for service: {v}")
+        
         if not any([v.startswith(prefix) for prefix in _VALID_URLS]):
+            logger.error(f"Invalid url for service: {v}")
             raise ValueError(f"Invalid URL for service: {v}")
+    
+    @field_validator("max_mb_per_file")
+    @classmethod
+    def validate_size(cls, v: Optional[float]) -> Optional[float]:
+        if not isinstance(v, (float, int)):
+            logger.error("Max file size should be float or integer")
+            raise ValueError("Max file size should be float or integer")
+        if not v > 0:
+            logger.error("Max file size logging should be positive")
+            raise ValueError("Max file size logging should be positive")
+        
 
         
 
